c01bf6363b6ba91c2ed4215ce9a89eecf00d10b3
tranngoctungchess-bit <tranngoctung.chess@gmail.com>
2026-05-01 17:24:20 +0700

    .

---
+++ likePy.h
@@ -0,0 +1,16 @@
+//
+// Created by Admin on 5/1/2026.
+//
+
+#ifndef UNTITLED_HAHA_H
+#define UNTITLED_HAHA_H
+#define import include
+#import <iostream>
+#define let auto
+#define print(x) std::cout<<x
+#define andprint(x) <<x
+#define input(x) std::cin>>x
+#define andinput(x) >>x
+#define MAIN int/**/main()
+typedef long long i64;
+#endif //UNTITLED_HAHA_H
---
+++ test.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Admin on 5/1/2026.
+//
+#import "likePy.h"
+MAIN{
+    i64 x;
+    input(x);
+    print("the number: ") andprint(x);
+}
